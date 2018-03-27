556425dbfcc911599e753d026d741fdc4892cdc2
Nicolas BOULFROY <nicolas.boulfroy@hotmail.fr>
2018-03-27 15:49:24 +0200

    Base view updated for FosUserBundle (code optimisation), base view updated for all others views, adminHomePage view updated (grapical interface improvement), prospectorHomepage view updated (graphical interface improvement), expenses view updated, prospecting view deleted (this feature will be not implement in the project), Profile view added (it displays the user's data), AppBundle controller updated (profile route added).

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,16 +5,35 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Profile;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         if ($this->getUser() != null || !empty($this->getUser())) {
             return $this->render('default/index.html.twig');
+        } else {
+            return $this->redirectToRoute('fos_user_security_login');
+        }
+    }
+
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function profileAction()
+    {
+        if ($this->getUser() != null || !empty($this->getUser())) {
+            $profile = $this->getDoctrine()->getRepository(Profile::class)->findBy(array(
+                'person' => $this->getUser()->getId()
+            ));
+
+            return $this->render('default/profile.html.twig', array(
+                'data' => $profile
+            ));
         } else {
             return $this->redirectToRoute('fos_user_security_login');
         }
